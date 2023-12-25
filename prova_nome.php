67f0915a7d652e5f5f2cf5871a972fef54ebe268
Larosa0Roberto <127388437+Larosa0Roberto@users.noreply.github.com>
2023-12-25 23:19:14 +0100

    ciao

---
+++ prova_nome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <title>Dati personali</title>
+</head>
+<body>
+  <form action="" method="post">
+    <input type="text" name="nome" placeholder="Nome">
+    <input type="text" name="cognome" placeholder="Cognome">
+    <input type="date" name="data_nascita">
+    <input type="submit" value="Invia">
+  </form>
+</body>
+</html>
